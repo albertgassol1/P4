879402f5b5727824b3fedc20696d74eb87218748
Albert Gassol Puigjaner <albertgassol1@gmail.com>
2020-05-02 17:42:39 +0200

    first commit

---
+++ graficos/coeficientes.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+file = open("lp1_2_3.txt", 'r')
+points = []
+
+f = file.read()
+lines = f.split('\n')
+
+for line in lines:
+    points.append([float(v) for v in line.split()])
+
+file.close()
+
+for point in points:
+    plt.axis('equal')
+    plt.plot(point[0],point[1], 'rx', markersize=4)
+
+plt.xlabel('a1')
+plt.ylabel('a2')
+plt.title('LPC')
+plt.grid(True)
+plt.show()
